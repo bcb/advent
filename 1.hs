a5622a2860b1deec1fd8b71686d670d8e0c67e64
Beau Barker <beauinmelbourne@gmail.com>
2020-12-05 14:06:51 +1100

    Add day 1

---
+++ 1.hs
@@ -0,0 +1,17 @@
+--- Day 1: Report Repair
+import Control.Applicative
+
+combinations :: [Int] -> [(Int, Int, Int)]
+combinations lst = liftA3 (,,) lst lst lst
+
+filtr :: [(Int, Int, Int)] -> [(Int, Int, Int)]
+filtr = filter (\(x, y, z) -> x + y + z == 2020)
+
+multiply :: [(Int, Int, Int)] -> [Int]
+multiply = fmap (\(x, y, z) -> x * y * z)
+
+main :: IO ()
+main = do
+    f <- readFile "1.txt"
+    let ints = fmap (read::String->Int) $ lines $ f
+    print $ head.multiply.filtr.combinations $ ints
