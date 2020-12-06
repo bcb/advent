ed7661d501a72792c3ab496a4816d14935217516
Beau Barker <beauinmelbourne@gmail.com>
2020-12-06 21:23:34 +1100

    Completed day 3

--- 3/3.hs
+++ 3/3.hs
@@ -1,10 +1,18 @@
 --- Day 3: Toboggan Trajectory
-getchar :: (Int, String) -> Char
-getchar (position, line) =
+
+getchar :: (String, Int) -> Char
+getchar (line, position) =
     let p = position `mod` (length line)
     in line !! p
+
+run :: [String] -> [Int] -> Int
+run slope positions = length.filter (== '#').fmap getchar $ zip slope positions
+
+everysecond (x:y:xs) = y:everysecond xs;
+everysecond _ = []
 
 main :: IO ()
 main = do
     input <- readFile "input"
-    print.length.filter (== '#').fmap getchar.zip [3,6..].tail.lines $ input
+    let slope = tail.lines $ input
+    print $ product $ fmap (run $ slope) [[1..],[3,6..],[5,10..],[7,14..]] ++ [run (everysecond $ slope) [1..]]
