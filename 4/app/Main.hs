cb0162ec26cc488a82110c9d8badc40bb22d2c9f
Beau Barker <beauinmelbourne@gmail.com>
2020-12-12 21:13:36 +1100

    Completed day 4

--- 4/app/Main.hs
+++ 4/app/Main.hs
@@ -6,6 +6,11 @@ import Data.Map as Map
 import Data.Validation
 import Text.Regex.PCRE
 
+newtype Error = Error [String] deriving Show
+
+instance Semigroup Error where
+    Error xs <> Error ys = Error (xs ++ ys)
+
 data Passport = Passport {
     byr :: String,
     iyr :: String,
@@ -16,50 +21,80 @@ data Passport = Passport {
     pid :: String
 } deriving (Show)
 
-newtype Error = Error [String] deriving Show
-
-maybeBool :: String -> Bool -> String -> Validation Error String
-maybeBool _ True s = Success s
-maybeBool err False _ =  Failure (Error [err])
-
-eqlen :: String -> Int -> String -> Validation Error String
-eqlen err l s = maybeBool err (length s == l) s
-
-minval :: String -> Int -> String -> Validation Error String
-minval err l s = maybeBool err ((read s::Int) <= l) s
-
-maxval :: String -> Int -> String -> Validation Error String
-maxval err l s = maybeBool err ((read s ::Int) >= l) s
-
-iselem :: String -> [String] -> String -> Validation Error String
-iselem err lst s = maybeBool err (elem s lst) s
-
-regex :: String -> String -> String -> Validation Error String
-regex err pattern s = maybeBool err (s =~ pattern) s
-
-validateByr :: String -> Validation Error String
-validateByr s =
-    case length s == 4 &&  minval "byr" 1920 <*> maxval "byr" 2002) of
+eqlen :: Int -> String -> Validation Error String
+eqlen l s =
+    case (length s == l) of
         True -> Success s
-        False -> Failure (Error [""])
+        False -> Failure (Error ["Incorrect length"])
 
-validateHgt :: String -> Validation Error String
-validateHgt s
-    | ("cm" `isSuffixOf` s) = return (takeWhile isDigit s) >>= minval "hgt" 150 >>= maxval "hgt" 193
-    | ("in" `isSuffixOf` s) = return (takeWhile isDigit s) >>= maxval "hgt" 59 >>= maxval "hgt" 76
+minval :: Int -> String -> Validation Error String
+minval l s =
+    case ((read s::Int) >= l) of
+        True -> Success s
+        False -> Failure (Error ["Below min val"])
+
+maxval :: Int -> String -> Validation Error String
+maxval l s =
+    case ((read s ::Int) <= l) of
+        True -> Success s
+        False -> Failure (Error ["Above max val"])
+
+iselem :: [String] -> String -> Validation Error String
+iselem lst s =
+    case (elem s lst) of
+        True -> Success s
+        False -> Failure (Error ["Not a valid value"])
+
+regex :: String -> String -> Validation Error String
+regex pattern s =
+    case (s =~ pattern) of
+        True -> Success s
+        False -> Failure (Error ["Does not match pattern"])
+
+
+validateByr :: Maybe String -> Validation Error String
+validateByr Nothing = Failure (Error ["byr missing"])
+validateByr (Just s) = eqlen 4 s *> minval 1920 s *> maxval 2002 s
+
+validateIyr :: Maybe String -> Validation Error String
+validateIyr Nothing = Failure (Error ["iyr missing"])
+validateIyr (Just s) = eqlen 4 s *> minval 2010 s *> maxval 2020 s
+
+validateEyr :: Maybe String -> Validation Error String
+validateEyr Nothing = Failure (Error ["eyr missing"])
+validateEyr (Just s) = eqlen 4 s *> minval 2020 s *> maxval 2030 s
+
+validateHgt :: Maybe String -> Validation Error String
+validateHgt Nothing = Failure (Error ["hgt missing"])
+validateHgt (Just s)
+    | ("cm" `isSuffixOf` s) = minval 150 digits *> maxval 193 digits
+    | ("in" `isSuffixOf` s) = minval 59 digits *> maxval 76 digits
     | otherwise = Failure (Error ["Doesn't end in cm or in"])
+    where digits = takeWhile isDigit s
+
+validateHcl :: Maybe String -> Validation Error String
+validateHcl Nothing = Failure (Error ["hcl missing"])
+validateHcl (Just s) = regex "^#[0-9a-f]{6}$" s
+
+validateEcl :: Maybe String -> Validation Error String
+validateEcl Nothing = Failure (Error ["ecl missing"])
+validateEcl (Just s) = iselem ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] s
+
+validatePid :: Maybe String -> Validation Error String
+validatePid Nothing = Failure (Error ["pid missing"])
+validatePid (Just s) = regex "^[0-9]{9}$" s
 
 -- Convert a mapping into a Passport type, validating each credential
-makePassport :: Map String String -> Maybe Passport
+makePassport :: Map String String -> Validation Error Passport
 makePassport map =
     Passport
         <$> validateByr (Map.lookup "byr" map)
-        <*> (Map.lookup "iyr" map >>= eqlen "iyr" 4 >>= minval "iyr" 2010 >>= maxval "iyr" 2020)
-        <*> (Map.lookup "eyr" map >>= eqlen "eyr" 4 >>= minval "eyr" 2020 >>= maxval "eyr" 2030)
-        <*> (Map.lookup "hgt" map >>= validateHgt)
-        <*> (Map.lookup "hcl" map >>= regex "hcl" "^#[0-9a-f]{6}$")
-        <*> (Map.lookup "ecl" map >>= iselem "ecl" ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])
-        <*> (Map.lookup "pid" map >>= eqlen "pid" 9)
+        <*> validateIyr (Map.lookup "iyr" map)
+        <*> validateEyr (Map.lookup "eyr" map)
+        <*> validateHgt (Map.lookup "hgt" map)
+        <*> validateHcl (Map.lookup "hcl" map)
+        <*> validateEcl (Map.lookup "ecl" map)
+        <*> validatePid (Map.lookup "pid" map)
 
 -- Separate a credential into a tuple, e.g. "byr:2003" -> ("byr", "2003")
 separate :: String -> (String, String)
@@ -71,8 +106,11 @@ separate str =
 parse :: String -> Map String String
 parse raw = Map.fromList $ separate <$> (words $ raw)
 
+isSuccess :: Validation a b -> Bool
+isSuccess (Failure _) = False
+isSuccess _ = True
+
 main :: IO ()
 main = do
     input <- readFile "input"
-    -- length.(filter isJust)
-    print $ makePassport.parse <$> splitOn "\n\n" input
+    print.length.Data.List.filter isSuccess.fmap (makePassport.parse) $ splitOn "\n\n" input
