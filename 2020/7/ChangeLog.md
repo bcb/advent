dc5b34f154e8ac6d2e5e83ffff98e8ce23eb3b5b
Beau Barker <beauinmelbourne@gmail.com>
2020-12-15 12:32:30 +1100

    Finished AOC day 7

---
+++ 2020/7/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for aoc202007
+
+## Unreleased changes
---
+++ 2020/7/app/Main.hs
@@ -0,0 +1,32 @@
+import Data.List.Split (splitOn)
+import Text.Regex.PCRE
+
+data Bag = Bag (String, [(String, Int)]) deriving Show
+
+findBag :: [Bag] -> String -> Bag
+findBag bags s = head $ filter (\(Bag (name, _)) -> name == s) bags
+
+carriesBag :: [Bag] -> Bag -> String -> Bool
+carriesBag bags (Bag (name, subBags)) s =
+    any (\(name, subbags) -> (name == s || (carriesBag bags (findBag bags name) s))) subBags
+
+-- Takes a string like "2 bright white bags" returning ("bright white", 2)
+parseSubBag :: String -> (String, Int)
+parseSubBag s =
+    let matches = s =~ "^([0-9]+) ([a-z ]+) bag" :: [[String]]
+    in (head.reverse.head $ matches, read.head.tail.head $ matches :: Int)
+
+parseContents :: String -> [(String, Int)]
+parseContents "no other bags." = []
+parseContents s = fmap parseSubBag $ splitOn ", " s
+
+parseBag :: String -> Bag
+parseBag s =
+    let parts = splitOn " bags contain " s
+    in Bag (head parts, parseContents $ head $ tail parts)
+
+main :: IO ()
+main = do
+    input <- readFile "input"
+    let bags = fmap parseBag.lines $ input
+    print.length.filter (\bag -> carriesBag bags bag "shiny gold") $ bags
