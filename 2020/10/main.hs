e4c1fc54f1d2f5f3fccecd505f4e90c668a9e7ef
Beau Barker <beauinmelbourne@gmail.com>
2020-12-19 10:16:00 +1100

    Completed day 10 part 2

--- 2020/10/main.hs
+++ 2020/10/main.hs
@@ -1,9 +1,18 @@
 import Data.List
 
+arrangements :: [Int] -> Int
+arrangements lst =
+    case lst of
+        1 : 1 : 1 : 1 : l -> arrangements l * 7
+        1 : 1 : 1 : l -> arrangements l * 4
+        1 : 1 : l -> arrangements l * 2
+        _ : l -> arrangements l
+        [] -> 1
+
 main :: IO ()
 main = do
     input <- readFile "input"
-    let all = fmap (read::String->Int).lines $ input
-    let allPlusBuiltin = sort $ [0, (maximum all) + 3] ++ all
-    let differences = fmap (\(x, y) -> y - x).zip allPlusBuiltin $ tail allPlusBuiltin
+    let all = sort $ (0:).fmap read.lines $ input
+    let differences = zipWith (-) (tail all) all
     print $ (length $ filter (==1) differences) * (length $ filter (==3) differences)
+    print $ arrangements differences
