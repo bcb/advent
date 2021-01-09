c0ac21c04947b50e4966da20bad5777fb0893040
Beau Barker <beauinmelbourne@gmail.com>
2021-01-10 00:52:49 +1100

    Completed day 17 part 1

--- 2020/17/main.hs
+++ 2020/17/main.hs
@@ -1,24 +1,42 @@
-newtype Coord = Coord (Int, Int, Int)
+import Data.Maybe (fromJust,isJust)
+
+newtype Coord = Coord (Int, Int, Int) deriving (Show, Eq)
+
+boardWidth = 7 -- Zero based
+boardHeight = 7 -- Zero based
+
+fst3,snd3,trd3 :: Coord -> Int
+fst3 (Coord (x,_,_)) = x
+snd3 (Coord (_,x,_)) = x
+trd3 (Coord (_,_,x)) = x
 
 countActiveNeighbours :: [Coord] -> [Coord] -> Int
-countActiveNeighbours neighbourCoords actives =
-    length.filter (==True).map (elem actives) $ neighbourCoords
+countActiveNeighbours actives neighbourCoords =
+    length.filter (==True).map (`elem` actives) $ neighbourCoords
 
 getNeighbourCoords :: Coord -> [Coord]
-getNeighbourCoords coord@(x,y,z) =
-    filter (==coord).[(i,j,k) | i <- [x-1..x+1], j <- [y-1..y+1], k <- [z-1..z+1]]
+getNeighbourCoords coord@(Coord (x,y,z)) =
+    filter (/=coord) $ [Coord (i,j,k) | i <- [x-1..x+1], j <- [y-1..y+1], k <- [z-1..z+1]]
 
-applyCube :: [Coord] -> Coord -> [Coord]
-applyCube actives coord =
-    let activeNeighbours = countActiveNeighbours (getNeighbourCoords coord) coord
-    in 
+applyCube :: [Coord] -> Coord -> Maybe Coord
+applyCube actives coord
+    | (coord `elem` actives) = if activeNeighbours `elem` [2,3] then Just coord else Nothing
+    | otherwise = if activeNeighbours == 3 then Just coord else Nothing
+    where activeNeighbours = countActiveNeighbours actives $ getNeighbourCoords coord
 
 step :: [Coord] -> Int -> [Coord]
-step actives =
-    concatMap (applyCube actives) actives
+step actives _ =
+    let minX = minimum.map fst3 $ actives
+        maxX = maximum.map fst3 $ actives
+        minY = minimum.map snd3 $ actives
+        maxY = maximum.map snd3 $ actives
+        minZ = minimum.map trd3 $ actives
+        maxZ = maximum.map trd3 $ actives
+    in
+    map fromJust.filter isJust $ [applyCube actives (Coord (x,y,z)) | x <- [minX-1 ..maxX+1], y <- [minY-1..maxY+1], z <- [minZ-1..maxZ+1]]
 
 main :: IO ()
 main = do
     input <- concat.lines <$> getContents
-    let actives = map fst.filter (\(_, c) -> c == '#').zip [(x,y,0) | x <- [0..2], y <- [0..2]] $ input
-    print $ foldl step actives [0..1]
+    let actives = map fst.filter (\(_, c) -> c == '#').zip [Coord (x,y,0) | x <- [0..boardWidth], y <- [0..boardHeight]] $ input
+    print.length $ foldl step actives [0..5]
