d0ead4c95ef69c76355ef78d22f43c158d815774
Beau Barker <beauinmelbourne@gmail.com>
2021-01-10 01:40:51 +1100

    Completed day 17 part 2

--- 2020/17/part2.hs
+++ 2020/17/part2.hs
@@ -3,20 +3,21 @@ import Data.Maybe (fromJust,isJust)
 boardWidth = 8
 boardHeight = 8
 
-data Coord = Coord Int Int Int deriving (Show, Eq)
+data Coord = Coord Int Int Int Int deriving (Show, Eq)
 
 getX, getY, getZ :: Coord -> Int
-getX (Coord x _ _) = x
-getY (Coord _ y _) = y
-getZ (Coord _ _ z) = z
+getX (Coord x _ _ _) = x
+getY (Coord _ y _ _) = y
+getZ (Coord _ _ z _) = z
+getW (Coord _ _ _ w) = w
 
 countActiveNeighbours :: [Coord] -> [Coord] -> Int
 countActiveNeighbours actives neighbourCoords =
     length.filter (==True).map (`elem` actives) $ neighbourCoords
 
 getNeighbourCoords :: Coord -> [Coord]
-getNeighbourCoords coord@(Coord x y z) =
-    filter (/=coord) $ [Coord i j k | i <- [x-1..x+1], j <- [y-1..y+1], k <- [z-1..z+1]]
+getNeighbourCoords coord@(Coord x y z w) =
+    filter (/=coord) $ [Coord i j k l | i <- [x-1..x+1], j <- [y-1..y+1], k <- [z-1..z+1], l <- [w-1..w+1]]
 
 applyCube :: [Coord] -> Coord -> Maybe Coord
 applyCube actives coord
@@ -26,13 +27,14 @@ applyCube actives coord
 
 step :: [Coord] -> Int -> [Coord]
 step actives _ =
-    map fromJust.filter isJust $ [applyCube actives (Coord x y z) |
+    map fromJust.filter isJust $ [applyCube actives (Coord x y z w) |
         x <- [(minimum.map getX $ actives)-1..(maximum.map getX $ actives)+1],
         y <- [(minimum.map getY $ actives)-1..(maximum.map getY $ actives)+1],
-        z <- [(minimum.map getZ $ actives)-1..(maximum.map getZ $ actives)+1]]
+        z <- [(minimum.map getZ $ actives)-1..(maximum.map getZ $ actives)+1],
+        w <- [(minimum.map getW $ actives)-1..(maximum.map getW $ actives)+1]]
 
 main :: IO ()
 main = do
     input <- concat.lines <$> getContents
-    let actives = map fst.filter (\(_, c) -> c == '#').zip [Coord x y 0 | x <- [0..boardWidth-1], y <- [0..boardHeight-1]] $ input
+    let actives = map fst.filter (\(_, c) -> c == '#').zip [Coord x y 0 0 | x <- [0..boardWidth-1], y <- [0..boardHeight-1]] $ input
     print.length $ foldl step actives [0..5]
