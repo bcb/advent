b43cbed95980a3c75a087f37138d74ab2f01b672
Beau Barker <beauinmelbourne@gmail.com>
2021-01-10 01:10:36 +1100

    Change newtype to data

--- 2020/17/part1.hs
+++ 2020/17/part1.hs
@@ -3,20 +3,20 @@ import Data.Maybe (fromJust,isJust)
 boardWidth = 7 -- Zero based
 boardHeight = 7 -- Zero based
 
-newtype Coord = Coord (Int, Int, Int) deriving (Show, Eq)
+data Coord = Coord Int Int Int deriving (Show, Eq)
 
 getX, getY, getZ :: Coord -> Int
-getX (Coord (x,_,_)) = x
-getY (Coord (_,y,_)) = y
-getZ (Coord (_,_,z)) = z
+getX (Coord x _ _) = x
+getY (Coord _ y _) = y
+getZ (Coord _ _ z) = z
 
 countActiveNeighbours :: [Coord] -> [Coord] -> Int
 countActiveNeighbours actives neighbourCoords =
     length.filter (==True).map (`elem` actives) $ neighbourCoords
 
 getNeighbourCoords :: Coord -> [Coord]
-getNeighbourCoords coord@(Coord (x,y,z)) =
-    filter (/=coord) $ [Coord (i,j,k) | i <- [x-1..x+1], j <- [y-1..y+1], k <- [z-1..z+1]]
+getNeighbourCoords coord@(Coord x y z) =
+    filter (/=coord) $ [Coord i j k | i <- [x-1..x+1], j <- [y-1..y+1], k <- [z-1..z+1]]
 
 applyCube :: [Coord] -> Coord -> Maybe Coord
 applyCube actives coord
@@ -26,7 +26,7 @@ applyCube actives coord
 
 step :: [Coord] -> Int -> [Coord]
 step actives _ =
-    map fromJust.filter isJust $ [applyCube actives (Coord (x,y,z)) |
+    map fromJust.filter isJust $ [applyCube actives (Coord x y z) |
         x <- [(minimum.map getX $ actives)-1..(maximum.map getX $ actives)+1],
         y <- [(minimum.map getY $ actives)-1..(maximum.map getY $ actives)+1],
         z <- [(minimum.map getZ $ actives)-1..(maximum.map getZ $ actives)+1]]
@@ -34,5 +34,5 @@ step actives _ =
 main :: IO ()
 main = do
     input <- concat.lines <$> getContents
-    let actives = map fst.filter (\(_, c) -> c == '#').zip [Coord (x,y,0) | x <- [0..boardWidth], y <- [0..boardHeight]] $ input
+    let actives = map fst.filter (\(_, c) -> c == '#').zip [Coord x y 0 | x <- [0..boardWidth], y <- [0..boardHeight]] $ input
     print.length $ foldl step actives [0..5]
