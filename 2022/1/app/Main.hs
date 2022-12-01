e0f3c6035baca1cd843884c3b41eb023b8dfaba0
Beau Barker <beau@explodinglabs.com>
2022-12-01 16:49:23 +1100

    Adjustment

--- 2022/1/app/Main.hs
+++ 2022/1/app/Main.hs
@@ -4,5 +4,5 @@ import Data.List.Split (splitOn)
 main :: IO ()
 main = do
   input <- readFile "input"
-  let elves = map sum . (map . map) (read :: String -> Int) . map words . splitOn "\n\n" $ input
-  print . sum . take 3 . reverse . sort $ elves
+  let elves = (map . map) (read :: String -> Int) . map words . splitOn "\n\n" $ input
+  print . sum . take 3 . reverse . sort . map sum $ elves
