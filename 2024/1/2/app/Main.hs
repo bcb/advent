7f82058a96f89ec5acdd6329ee6c58974dd8ed7c
Beau Barker <beau@explodinglabs.com>
2024-12-01 20:21:46 +1100

    Update 2024/1/2

--- 2024/1/2/app/Main.hs
+++ 2024/1/2/app/Main.hs
@@ -1,13 +1,13 @@
 import Data.List (sort)
 import Data.List.Split (splitOn)
 
-occurrences :: [Int] -> Int -> Int
+occurrences :: Eq a => [a] -> a -> Int
 occurrences lst x = length . filter (x ==) $ lst
 
 main :: IO ()
 main = do
   lst <- readFile "input"
   let splitted = map (splitOn "   ") . lines $ lst
-      fsts = map (\x -> read x :: Int) . map head $ splitted
-      snds = map (\x -> read x :: Int) . map (!! 1) $ splitted
-  print . sum . map (\x -> x * occurrences snds x) $ fsts
+      fsts = map head $ splitted
+      snds = map (!! 1) $ splitted
+  print . sum . map (\x -> (read x :: Int) * occurrences snds x) $ fsts
